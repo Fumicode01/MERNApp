fc38ca0fe37264ca540c514a17d834e6da4029e2
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-10-26 23:36:10 +1100

    Connect to MongoDB
    Created MongoDB cluster and DB
    connected to Mongo DB

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,6 +7,21 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {userNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log("connected to MongoDB");
+});
+
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to homepage")
+})
+
+
 
 app.listen(8800, () =>{
     console.log("BackEnd server is running"
